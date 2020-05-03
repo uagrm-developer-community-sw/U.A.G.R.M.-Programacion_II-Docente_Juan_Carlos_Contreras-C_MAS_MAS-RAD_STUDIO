a2a01189b27f69929280f25b92d79c0445999ec5
jhasmany_jhunnior@outlook.com <jhasmany>
2020-05-02 21:48:42 -0400

    subiendo archivos

---
+++ UnitPrincipal.h
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------
+
+#ifndef UnitPrincipalH
+#define UnitPrincipalH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Imaging.pngimage.hpp>
+//---------------------------------------------------------------------------
+class TFormPrincipal : public TForm
+{
+__published:	// IDE-managed Components
+	TPanel *PanelPrincipal;
+	TImage *ImagePrincipal;
+	TButton *BtnNumeros;
+	TButton *BtnSeries;
+	TButton *BtnCadenas;
+	TButton *BtnVectores;
+	TButton *BtnMatrices;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TFormPrincipal(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TFormPrincipal *FormPrincipal;
+//---------------------------------------------------------------------------
+#endif
